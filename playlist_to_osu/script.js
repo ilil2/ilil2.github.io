f895b8e7528f6319e0cb34bf8790ce61adf7ba73
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-12-17 18:47:34 +0100

    beautiful archi

---
+++ playlist_to_osu/script.js
@@ -0,0 +1,36 @@
+const urlParams = new URLSearchParams(window.location.search);
+const playlistsId = urlParams.get("playlistsId");
+let hasExecuted = false;
+
+if (playlistsId && !hasExecuted) {
+    if (!sessionStorage.getItem("playlistsData")) {
+        fetch(`https://playlist-to-osu.onrender.com/api/spotify-playlists?playlistsId=${playlistsId}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Playlists:", data); // Affiche les playlists dans la console
+                sessionStorage.setItem("playlistsData", JSON.stringify(data)); // Stocke les données dans sessionStorage
+                displayPlaylists(data.items); // Affiche les playlists dans la page
+            })
+            .catch(error => console.error("Error fetching playlists:", error));
+    }
+    else {
+        // Charge les données à partir de sessionStorage si elles sont déjà présentes
+        const cachedData = JSON.parse(sessionStorage.getItem("playlistsData"));
+        console.log("Cached Playlists:", cachedData);
+        displayPlaylists(cachedData.items); // Affiche les playlists dans la page
+    }
+
+    hasExecuted = true;
+    //urlParams.delete("playlistsId");
+    //window.location.href = window.location.pathname;
+}
+
+function displayPlaylists(playlists) {
+    const container = document.getElementById("playlists");
+    container.innerHTML = "";
+    playlists.forEach(playlist => {
+        const div = document.createElement("div");
+        div.textContent = playlist.name;
+        container.appendChild(div);
+    });
+}
